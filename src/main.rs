ec1fdc228c5d42c01c82bf8e74529f34382a7b88
lessan-cyber <azizmetchonou434@gmail.com>
2026-03-24 23:00:09 +0000

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod cli;
+mod ping;
+
+use cli::Args;
+use colored::Colorize;
+//use ping::PingStats;
+
+fn main() {
+    let args = Args::new();
+    println!("{}", args.simple);
+
+    match ping::measure_ping_and_jitter("8.8.8.8:53", 5) {
+        Ok(stats) => {
+            println!("Average Ping: {:.3} ms", stats.average_ping);
+            println!("Average Jitter: {:.3} ms", stats.average_jitter);
+        }
+        Err(_) => {
+            // Print to standard error instead of standard out
+            eprintln!(
+                "{}",
+                "You are not even connected to the internet, anyways it's not that deep go touch some grass".red().bold()
+            );
+            // Exit gracefully with an error code (1)
+            std::process::exit(1);
+        }
+    };
+}
